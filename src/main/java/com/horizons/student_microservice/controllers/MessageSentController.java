c331ab95255eac55a167f2107ad32af0a5596b74
Rachid Abadli <rabadli@localhost.localdomain>
2020-08-22 03:35:17 -0700

    Fixed messages sent

--- src/main/java/com/horizons/student_microservice/controllers/MessageSentController.java
+++ src/main/java/com/horizons/student_microservice/controllers/MessageSentController.java
@@ -1,9 +1,13 @@
 package com.horizons.student_microservice.controllers;
 
+import com.horizons.student_microservice.models.MessageSent;
 import com.horizons.student_microservice.repositories.MessageSentRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/messages-sent")
@@ -11,4 +15,31 @@ public class MessageSentController {
     @Autowired
     private MessageSentRepository repository;
 
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<MessageSent> getAllMessagesSent() {
+        return repository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public MessageSent getMessageSentById(@PathVariable("id") ObjectId id) {
+        return repository.findById(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public void modifyStudentById(@PathVariable("id") ObjectId id, @Valid @RequestBody MessageSent messageSent) {
+        messageSent.setId(id);
+        repository.save(messageSent);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public MessageSent createMessageSent( @Valid @RequestBody MessageSent messageSent) {
+        messageSent.setId(ObjectId.get());
+        repository.save(messageSent);
+        return messageSent;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteMessageSent(@PathVariable ObjectId id) {
+        repository.delete(repository.findById(id));
+    }
 }
--- src/main/java/com/horizons/student_microservice/models/MessageSent.java
+++ src/main/java/com/horizons/student_microservice/models/MessageSent.java
@@ -7,7 +7,40 @@ import java.util.Date;
 
 public class MessageSent {
     @Id
-    private ObjectId messageSentId;
+    private ObjectId id;
     private Date dateSent;
     private ObjectId messageId;
+
+    public MessageSent() {
+    }
+
+    public MessageSent(ObjectId id, Date dateSent, ObjectId messageId) {
+        this.id = id;
+        this.dateSent = dateSent;
+        this.messageId = messageId;
+    }
+
+    public String getId() {
+        return id.toHexString();
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public Date getDateSent() {
+        return dateSent;
+    }
+
+    public void setDateSent(Date dateSent) {
+        this.dateSent = dateSent;
+    }
+
+    public ObjectId getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(ObjectId messageId) {
+        this.messageId = messageId;
+    }
 }
